e92f389d6577952bfde12c1951a7b0e5f4f777d1
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-05-26 14:23:17 +0200

    add roles in login response

---
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,34 @@
+<?php
+
+// src/App/EventListener/AuthenticationSuccessListener.php
+// permet d'ajouter des données au token renvoyé au login
+namespace App\EventListener;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+
+class AuthenticationSuccessListener
+{
+
+    /**
+    * @param AuthenticationSuccessEvent $event
+    */
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+
+        $data = $event->getData();
+        $user = $event->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return;
+        }
+
+        $data['data'] = array(
+            'roles' => $user->getRoles(),
+        );
+
+        $event->setData($data);
+
+    }
+
+}
